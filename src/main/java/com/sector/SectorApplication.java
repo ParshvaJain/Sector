74a0b6a29b46ee2767202ae24ce3ce8270afc52f
ParshvaJain <pashwab255225@gmail.com>
2021-06-21 11:53:36 +0530

    third commit

--- src/main/java/com/sector/SectorApplication.java
+++ src/main/java/com/sector/SectorApplication.java
@@ -2,7 +2,7 @@ package com.sector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 public class SectorApplication {
